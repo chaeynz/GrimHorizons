ce331d42fec5063235ee93903dfa88e871fa4467
chaeynz <kevin@danielpuls.com>
2023-07-09 13:34:43 +0200

    first functionality Items and Inventory

---
+++ src/Globals.cpp
@@ -0,0 +1,7 @@
+#include "Item.hpp"
+//#include "Enemy.hpp"
+//#include "Player.hpp"
+
+int Item::lastAssignedId = 0;
+//int Enemy::lastAssignedId = 0;
+//int Player::lastAssignedId = 0;
---
+++ src/Rarity.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+enum class Rarity {
+	Common,
+	Uncommon,
+	Rare,
+	Epic,
+	Legendary,
+	Special
+};
